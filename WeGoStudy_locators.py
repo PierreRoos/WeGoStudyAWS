857aca7907c3fa46dab8799bf608691950230264
Evamuriithi <101294849+Evamuriithi@users.noreply.github.com>
2022-05-17 09:54:24 -0800

    first commit

--- WeGoStudy_locators.py
+++ WeGoStudy_locators.py
@@ -1,40 +1,59 @@
 import datetime
-import locale
+from faker import Faker
+fake = Faker(locale='en_CA')
 import random
 
-from faker import Faker
-faker = Faker()
 
 
 app = 'WeGoStudy'
-app_url = 'http://34.233.225.85/'
-homepage_title = 'WeGoStudy'
+wegostudy_url = 'http://34.233.225.85/'
+wegostudy_home_page_title = 'WeGoStudy'
+wegostudy_login_page_url = 'http://34.233.225.85/partners/student_details/new'
+wegostudy_student_login_page_url = 'http://34.233.225.85/partners/student_details/new'
+new_username = fake.user_name()
+new_password = fake.password()
+first_name = fake.first_name()
+last_name = fake. last_name()
+middle_name = fake.first_name()
+full_name = f'{first_name} {last_name}'
 admin_email = 'chris.velasco78@gmail.com'
 admin_password = '123cctb'
-login_page_url = 'http://34.233.225.85/partner/home'
-student_page_url = 'http://34.233.225.85/partners/student_details'
+email = fake.email()
 
-first_name = faker.first_name()
-last_name = faker.last_name()
-birthday = str(faker.date_of_birth())
-passport_num = faker.pyint(1111111, 9999999)
-country = faker.country()
-state = faker.state()
-city = faker.city()
-address = faker.street_address()
-zip_code = faker.zipcode()
-phone_num = faker.pyint(1111111111, 9999999999)
-email = faker.email()
-school = f'{country} University'
-program = random.choice(['Computer', 'Physics', 'Statistic', 'Math', 'Language', 'Chemistry', 'Medicine'])
-gpa = faker.pyint(1,100)
+country_citizenship = fake.country()
+phone_number = fake.pyint(11111111111, 77777777777)
+address = fake.address()
+country = fake.current_country()
+country2 = fake.country()
+city = fake.city()
+province = fake.province_abbr()[0:10]
+postalcode = fake.postcode()[0:10]
+school = f'{province} University'
+program = random.choice(['Computer', 'Physics', 'Statistic', 'Math', 'Biologist', 'Chemistry', 'Architecture'])
+gpa = fake.pyint(1,100)
+date_of_birth = fake.date()[0:50]
+passport_number = fake.pyint(1111111, 7777777)
+credentials = random.choices(['Degree', 'Certificate', 'Diploma', 'Doctoral'])
+schools = random.choices(['Alexander College', 'Acsenda School of Management', 'Capilano University','Columbia College', 'Durham College', 'Okanagan College'])
+gpa_scale = fake.pyint(1,100)
+
 
 lst_column = ['First Name', 'Last Name', 'Passport Number', 'Phone Number', 'Mailing Address', 'Zip Code', 'Email', 'School Name', 'Program', 'GPA']
-lst_id = ['user_student_detail_attributes_first_name', 'user_student_detail_attributes_last_name',
+
+lst_id = ['user_student_detail_attributes_first_name',
+          'user_student_detail_attributes_last_name',
           'user_student_detail_attributes_passport_number', 'phone_number',
           'user_student_detail_attributes_address_attributes_mailing_address',
           'user_student_detail_attributes_address_attributes_zip_code', 'user_email',
           'user_student_detail_attributes_user_educations_attributes_0_school_name',
           'user_student_detail_attributes_user_educations_attributes_0_program',
           'user_student_detail_attributes_user_educations_attributes_0_gpa']
-lst_value = [first_name, last_name, passport_num, phone_num, address, zip_code, email, school, program, gpa]
+
+lst_value = [first_name, last_name, passport_number, phone_number, address, postalcode, email, school, program, gpa]
+
+# lst_column2 = ['Institute Name','Course name','Starting Semester','Start Day','Start Year']
+#
+# lst_id2 = ['admission_institute_detail_id_chosen','admission_institute_program_id_chosen',
+#            'admission_starting_semester_chosen','admission_start_day_chosen','admission_start_year_chosen']
+#
+# lst_value2 = [2, 3, 2, 1, 3]
